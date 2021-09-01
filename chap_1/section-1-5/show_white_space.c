956f11516d3bb248de1683a2f88b34b71c5f0162
Lyon <coderlyonma@gmail.com>
2021-09-01 18:59:36 +0800

    reformat the dir tree

---
+++ chap_1/section-1-5/show_white_space.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    char c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\t");
+            continue;
+        }
+        else if (c == '\b') {
+            printf("\\b");
+            continue;
+        }
+        else if (c == '\\') {
+            printf("\\\\");
+            continue;
+        }
+        putchar(c);
+    }
+}
+
