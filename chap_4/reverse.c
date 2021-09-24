1f0d95a159cf478a0160be5f3ce77505fc15b43e
Lyon <coderlyonma@gmail.com>
2021-09-24 11:35:04 +0800

    reverse recursively

--- chap_4/reverse.c
+++ chap_4/reverse.c
@@ -2,29 +2,30 @@
 #define MAXC 1000
 
 int str_len(char *s);
-void reverse(char s[]);
+void reverse(char s[], int l, int r);
 
 
 int main()
 {
     char s_1[] = "hello world"; 
-    reverse(s_1);
+    reverse(s_1, 0, str_len(s_1) - 1);
     printf("%s\n", s_1);
-    //printf("%d\n", str_len(s_1));
+//    printf("%d\n", str_len(s_1));
 }
 
 
-void reverse(char s[])
+void reverse(char s[], int l, int r)
 {
     char tmp;
 
-    if (str_len(s) != 1) {
-        tmp = *(s+1);
-        *(s+1) = *s;
-        *s = tmp;
+    if (r - l <= 0) {
+        return;
     }
 
-    reverse((s + 1));
+    tmp = s[l];
+    s[l] = s[r];
+    s[r] = tmp;
+    reverse(s, l+1, r-1);
 }
 
 int str_len(char *s)
