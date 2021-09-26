ef2efac96810e1f14f9fb07132e73a8785bbd913
Lyon <coderlyonma@gmail.com>
2021-09-26 17:47:45 +0800

    utils n_strcmp bug fixed

--- chap_5/sort_lines/utils.c
+++ chap_5/sort_lines/utils.c
@@ -8,13 +8,18 @@ int strcmp(char* s, char* t)
     return *s - *t;
 }
 
-/* 为什么该函数的实现是错误的？ */
+/* 
+为什么该函数的实现是错误的？
+因为当第一次匹配到不相等的字符之后，下一个循环虽然得到了false，但是指针又向前移动了一次
+解决方案，指针向前回退一次即可
+*/
+
 int n_strcmp(char* s, char* t)
 {
     while (*s++ == *t++)
         ;
 
-    return *s - *t;
+    return *(--s) - *(--t);
 }
 
 void swap(char* s[], int i, int j)
