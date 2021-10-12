50ec63998d021c6523a790a29c70ef9bf6509e2f
Lyon <coderlyonma@gmail.com>
2021-10-12 17:12:06 +0800

    detab done

--- chap_1/section-1-10/detab.c
+++ chap_1/section-1-10/detab.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
-
+#define MAX_LEN 1000
 #define TABW 8
+
 
 int main()
 {
     int c, pos;
-
+    
     pos = 1;
     while ((c = getchar()) != EOF) {
         if (c == '\n') {
-            pos = 1;
-            putchar(c);
-        }
-        else if (c == '\t') {
-            for (int ns = TABW - ((pos - 1) % TABW); ns > 0; --ns) {
-                ++pos;
-                putchar(' ');
-            }
-        }
-        else {
+            pos = 1, putchar(c);
+        } else if (c == '\t') {
+            int ns = TABW - ((pos - 1) % TABW);
+            for (; ns > 0; --ns, putchar(' '), ++pos)
+                ;
+        } else {
             ++pos;
             putchar(c);
         }
     }
-
-    return 0;
 }
 
